df0f056523534a802337f4bd6d565c66a41910b3
afsaanchez04 <afsanchez04@gmail.com>
2024-08-07 00:23:11 -0500

    Clase 1: intro js

---
+++ 01-sintaxis-variables/app.js
@@ -0,0 +1,23 @@
+
+/* variables
+javascript
+operadores */
+
+var bolsa = 'manzanas'
+
+var caja
+caja = 'mangos'
+
+/* console.log( typeof caja ) */
+
+var bicicleta = {
+    color: 'negro',
+    ruedas: 2,
+    velocidades: 5,
+    frenos: 'disco',
+    marca: 'BMW'
+}
+
+/* console.log( "Marca:",bicicleta.marca ) */
+
+console.log( 9*3 )
