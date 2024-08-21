ef596ecf581efa7c1e6360d2c9551a519760846e
afsaanchez04 <afsanchez04@gmail.com>
2024-08-20 22:44:58 -0500

    feat: dom

---
+++ 07-dom/app.js
@@ -0,0 +1,121 @@
+
+//Ejercicio captura de variables
+
+/* const sectionIntro = document.getElementById("intro")
+const titulo = document.getElementsByTagName('h1')
+const parrafos = document.getElementsByTagName('p')
+const botones = document.getElementsByClassName('btn')
+const aviso = document.querySelector('#aviso')
+const miParrafo = document.querySelector('.parrafo')
+const btnApp = document.querySelectorAll('.btn')
+
+
+const listaBotones = [...botones]
+
+console.log(sectionIntro)
+console.log(titulo[0])
+console.log(parrafos[1])
+console.log(botones)
+console.log(aviso)
+console.log(miParrafo)
+console.log(btnApp)
+
+listaBotones.forEach(element => {
+    element.style.backgroundColor = "orange"
+}); 
+
+function consultarLibros(libro){
+    alert(`Buscando libro "${libro}"... 🤖`)
+    titulo[0].innerText = `El libro "${libro}" no se encontró 😔`
+} */
+
+//Ejercicio contador
+//Programar un contador que incremente hasta 20 y que no sea inferior a 0
+
+/* const contadorElement = document.getElementById('contador');
+const btnMas = document.getElementById('btnMas');
+const btnMenos = document.getElementById('btnMenos');
+let contador = 0;
+
+btnMas.addEventListener('click', () => {
+    if (contador < 20) {
+        contador++;
+        contadorElement.textContent = contador;
+    }
+});
+
+btnMenos.addEventListener('click', () => {
+    if (contador > 0) {
+        contador--;
+        contadorElement.textContent = contador;
+    }
+}); */
+
+
+//Ejercicio animación pelota
+//Pelota cambia de color y se mueve en horizontal a la derecha
+
+//1. Captura de variables
+const cantPixeles = document.querySelector("#pixeles")
+const listaColores = document.querySelector("#listaColores")
+const formulario = document.querySelector("#formulario")
+const pelota = document.querySelector(".pelota")
+
+//2. Creación de funciones
+function moverPelota(e){
+    e.preventDefault()
+    pelota.style.backgroundColor = listaColores.value
+    pelota.style.marginLeft = cantPixeles.value + 'px'
+    pelota.style.transitionDuration = '.3s'
+    console.log('Ejecutando...')
+}
+
+function desaparecer (){
+    pelota.style.display = "none"
+}
+
+//3. Llamado del evento
+formulario.addEventListener('submit',moverPelota)
+pelota.addEventListener("click",desaparecer)
+
+
+//Validación usuario
+
+var cuentas = [
+    { nombre: 'Mali', saldo: 200, pass: 12345 },
+    { nombre: 'Gera', saldo: 290, pass: 12345 },
+    { nombre: 'Maui', saldo: 67, pass: 12345 }
+];
+
+//1. Variables
+const formInicio = document.querySelector('#formInicio')
+const userName = document.querySelector('#user')
+const passName = document.querySelector('#password')
+
+//2. Funciones
+function validarUser (e){
+    e.preventDefault()
+    cuentas.forEach( user => {
+        let validacionUser = userName.value.toLowerCase() === user.nombre.toLowerCase()
+        let validacionPass = passName.value == user.pass
+
+        console.log(typeof user.pass)
+        console.log(typeof passName.value)
+
+        if(validacionUser && validacionPass){
+            console.log(`Bienvenido ${user.nombre}`)  
+            return
+        }else{
+            console.log(`Datos incorrectos ✖️`)
+            return
+        }
+        
+    } )
+    
+    
+
+        
+}
+
+//3. Evento 
+formInicio.addEventListener('submit',validarUser)
